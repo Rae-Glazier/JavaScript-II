3a6deba3b11904ed68202594c014630833956635
Rae Glazier <glazier.home.life@gmail.com>
2019-10-20 11:16:13 -0500

    support hours work

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -58,14 +58,20 @@ const runners = [
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs. Combine both the first and last names and populate a new array called `fullNames`. This array will contain just strings.
 let fullNames = [];
-//console.log(fullNames);
+
+runners.forEach( run => {
+  let addName = `${run.first_name} ${run.last_name}`;
+  fullNames.push(addName)
+})
+
+console.log(fullNames);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runners' first names in uppercase because the director BECAME DRUNK WITH POWER. Populate an array called `firstNamesAllCaps`. This array will contain just strings.
 let firstNamesAllCaps = [];
-firstNamesAllCaps = runners.map(runners => {
-  return runners.first_name.toUpperCase();
-});
+firstNamesAllCaps = runners.map(caps => 
+  caps.first_name.toUpperCase()
+)
 console.log(firstNamesAllCaps);
 
 // ==== Challenge 3: Use .filter() ====
